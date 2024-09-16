77b7023560deba6fb975800ba1326c1c573bf495
Glenn <glenndavidmah@gmail.com>
2024-09-15 19:20:13 -0700

    adding file folder

---
+++ classwork7/Classwork7.cpp
@@ -0,0 +1,18 @@
+// Classwork7.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string names[3];
+	cout << "Please input the first name\n";
+	cin >> names[0];
+	cout << "Please input the second name\n";
+	cin >> names[1];
+	cout << "Please input the third name\n";
+	cin >> names[2];
+
+	cout << names[1];
+}
